1cd74c0b90d20701a7ac67de74244b1717a187a0
stevenlordiam <stevenlordiam@gmail.com>
2015-08-30 10:56:19 -0400

    add 1 answer

--- Algorithm/MergeSort.java
+++ Algorithm/MergeSort.java
@@ -8,21 +8,21 @@ import java.util.*;
 
 public class MergeSort {	
 	public static void mergesort(int[] array) {
-		int[] helper = new int[array.length];
-		mergesort(array, helper, 0, array.length - 1);
+		mergesort(array, 0, array.length - 1);
 	}
 
-	public static void mergesort(int[] array, int[] helper, int low, int high) {
+	public static void mergesort(int[] array, int low, int high) {
 		if (low < high) {
 			int middle = (low + high) / 2;
-			mergesort(array, helper, low, middle); // Sort left half
-			mergesort(array, helper, middle+1, high); // Sort right half
-			merge(array, helper, low, middle, high); // Merge them
+			mergesort(array, low, middle); // Sort left half
+			mergesort(array, middle+1, high); // Sort right half
+			merge(array, low, middle, high); // Merge them
 		}
 	}
 
-	public static void merge(int[] array, int[] helper, int low, int middle, int high) {
-		// Copy both halves into a helper array
+	public static void merge(int[] array, int low, int middle, int high) {
+		// Copy both halves into a helper array, remember: cannot simply do "helper = array"
+		int[] helper = new int[array.length]; 
 		for (int i = low; i <= high; i++) {
 			helper[i] = array[i];
 		}
